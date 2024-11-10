34d06a80b3fdc2e4f937652f27bce5edfe8c0d89
mujtaba024 <162799629+mujtaba024@users.noreply.github.com>
2024-11-10 16:51:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+var _a;
+//lisiting element
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
+    event.preventDefault();
+    //type assertion
+    const profilePictureInput = document.getElementById('profilePicture');
+    const nameElement = document.getElementById('name');
+    const emailElement = document.getElementById('email');
+    const phoneElement = document.getElementById('phone');
+    const educationElement = document.getElementById('education');
+    const experienceElement = document.getElementById('experience');
+    const skillsElement = document.getElementById('skills');
+    if (profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        const name = nameElement.value;
+        const email = educationElement.value;
+        const phone = phoneElement.value;
+        const education = educationElement.value;
+        const experience = experienceElement.value;
+        const skills = skillsElement.value;
+        // picture element
+        const profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+        //creat resume output
+        const resumeOutput = `
+    <h2>Resume</h2>
+    ${profilePictureURL ? '<img src=${profilePictureURL}alt=profile picture" class="profilePicture">' : ''}
+    <p><strong>Name:</strong> ${name} </p>
+    <p><strong>Email:</strong> ${email} </p>
+    <p><strong>Phone Number:</strong> ${phone} </p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</p>
+    `;
+        const resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        }
+        else {
+            console.error('the resume output element are missing');
+        }
+    }
+    else {
+        console.error('one or more output element are missing');
+    }
+});
